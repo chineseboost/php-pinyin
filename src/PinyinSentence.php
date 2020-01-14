3aa0e1e02456bd1f786bf72979449ced6f05c6a6
Hugh Grigg <hugh@freetrade.io>
2020-01-14 20:38:24 +0000

    Pinyin sentence parse WIP

--- src/PinyinSentence.php
+++ src/PinyinSentence.php
@@ -2,6 +2,81 @@
 
 namespace Pinyin;
 
-class PinyinSentence
+use Pinyin\String\Normalizing;
+
+class PinyinSentence implements Normalizing
 {
+    /** @var string */
+    private $sentence;
+
+    /** @var int */
+    private $wordLimit;
+
+    /**
+     * @param string $sentence
+     * @param int $wordLimit
+     */
+    public function __construct(string $sentence, int $wordLimit = 1000)
+    {
+        $this->sentence = $sentence;
+        $this->wordLimit = $wordLimit;
+    }
+
+    public function normalized(): Normalizing
+    {
+        return new static(
+            preg_replace(
+                '/\s+/u', ' ',
+                PinyinRegex::normalize($this->sentence)
+            )
+        );
+    }
+
+    /**
+     * @return Normalizing[]
+     */
+    public function elements(): array
+    {
+        $words = [];
+        $remaining = $this->sentence;
+        $currentWord = "";
+
+        for ($i = 0; mb_strlen($remaining) > 0 && $i < $this->wordLimit; $i++) {
+            $nextSyllable = PinyinRegex::extractFirstSyllable($remaining);
+            if (!$nextSyllable) {
+                if (mb_strlen($remaining) > 0) {
+                    array_push($elements, new NonPinyinString($remaining));
+                }
+                break;
+            }
+
+            $nextSyllablePos = mb_strpos($remaining, $nextSyllable);
+            if ($nextSyllablePos !== 0) {
+                $nonSyllable = mb_substr($remaining, 0, $nextSyllablePos);
+                array_push($elements, new NonPinyinString($nonSyllable));
+                $remaining = mb_substr($remaining, mb_strlen($nonSyllable));
+                continue;
+            }
+
+            $nextSyllableFirstLetter = mb_substr($nextSyllable, 0, 1);
+            if (mb_strlen($currentWord) === 0
+                || $nextSyllableFirstLetter === mb_strtoupper($nextSyllableFirstLetter)
+            ) {
+                $currentWord .= " ${nextSyllable} ";
+                $remaining = mb_substr($remaining, mb_strlen($nextSyllable));
+                continue;
+            }
+
+            array_push($elements, new PinyinWord(trim($currentWord)));
+            $currentWord = '';
+            $remaining = mb_substr($remaining, mb_strlen($nextSyllable));
+        }
+
+        return $words;
+    }
+
+    public function __toString(): string
+    {
+        return $this->sentence;
+    }
 }
---
+++ tests/Unit/PinyinSentence/PinyinSentenceElementsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Pinyin\Tests\Unit\PinyinSentence;
+
+use PHPUnit\Framework\TestCase;
+use Pinyin\PinyinSentence;
+
+class PinyinSentenceElementsTest extends TestCase
+{
+    /**
+     * @dataProvider elementsProvider
+     *
+     * @param string $sentence
+     * @param array $expectedElements
+     */
+    public function testElements(string $sentence, array $expectedElements)
+    {
+        // Given we have a pinyin sentence;
+        $pinyinSentence = new PinyinSentence($sentence);
+
+        // When we get its component elements;
+        $elements = $pinyinSentence->elements();
+
+        // Then we should get the correct elements.
+        self::assertCount(
+            count($expectedElements),
+            $elements,
+            sprintf(
+                '"%s" should have %d elements ("%s"), got %d ("%s")',
+                $sentence,
+                count($expectedElements),
+                implode('","', $expectedElements),
+                count($elements),
+                implode('","', $elements)
+            )
+        );
+        foreach ($expectedElements as $i => $expectedElement) {
+            self::assertSame(
+                (string) $expectedElement,
+                (string) ($elements[$i]),
+                sprintf(
+                    '#%d word of "%s" should be "%s", got "%s"',
+                    $i,
+                    $sentence,
+                    $expectedElement,
+                    $elements[$i]
+                )
+            );
+        }
+    }
+
+    /**
+     * @return array[]
+     */
+    public function elementsProvider(): array
+    {
+        return [
+            ['Wo3 yao4 qu4 Bei3jing1 le', ['Wo3', 'yao4', 'qu4', 'Bei3jing1', 'le']]
+        ];
+    }
+}
