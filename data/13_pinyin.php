5a69f46a438fa650c4ad9e4b7e5f6311babe1bb0
Hugh Grigg <hugh.grigg@gmail.com>
2020-03-29 16:01:25 +0000

    Apply fixes from StyleCI

--- data/13_pinyin.php
+++ data/13_pinyin.php
@@ -1,1 +1,1 @@
-<?php return ['全國人民代表大會常務委員會'=>'Quan2guo2 Ren2min2 Dai4biao3 Da4hui4 Chang2wu4 Wei3yuan2hui4','全国人民代表大会常务委员会' => 'Quan2guo2 Ren2min2 Dai4biao3 Da4hui4 Chang2wu4 Wei3yuan2hui4',];
+<?php return ['全國人民代表大會常務委員會'=>'Quan2guo2 Ren2min2 Dai4biao3 Da4hui4 Chang2wu4 Wei3yuan2hui4', '全国人民代表大会常务委员会' => 'Quan2guo2 Ren2min2 Dai4biao3 Da4hui4 Chang2wu4 Wei3yuan2hui4'];
