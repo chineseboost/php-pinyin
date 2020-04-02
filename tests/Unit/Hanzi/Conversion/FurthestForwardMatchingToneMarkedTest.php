68d6cf21058f643654d66da111eb05b0d191427b
Hugh Grigg <hugh.grigg@gmail.com>
2020-04-02 21:29:14 +0000

    Apply fixes from StyleCI

--- tests/Unit/Hanzi/Conversion/FurthestForwardMatchingToneMarkedTest.php
+++ tests/Unit/Hanzi/Conversion/FurthestForwardMatchingToneMarkedTest.php
@@ -72,7 +72,7 @@ class FurthestForwardMatchingToneMarkedTest extends TestCase
             [
                 '你想吃苹果吗？',
                 'Nǐ xiǎng chī píngguǒ ma?',
-            ]
+            ],
         ];
     }
 }
